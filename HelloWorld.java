77fac843112d4f5b0471d3eae5f34c109bb081d2
Fernando Ontiveros <feronti@gmail.com>
2016-10-08 09:56:18 -0400

    cambio simple

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,6 @@
 
      // A Java program begins with a call to main().
      public static void main(String args[]) {
-       System.out.println("Hello from Java!!");
+       System.out.println("Hello from Java Saturday!!");
      }
     }
